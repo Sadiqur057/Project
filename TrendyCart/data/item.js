43b512b4cb345cad926cf79dae3655f0d43130af
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-25 18:38:31 +0600

    data declared as object

---
+++ TrendyCart/data/item.js
@@ -0,0 +1,81 @@
+const items = [
+    {
+        id: '001',
+        itemImage: './images/item-13.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42
+    },
+    {
+        id: '002',
+        itemImage: './images/item-25.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42
+    },
+    {
+        id: '003',
+        itemImage: './images/item-27.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42
+    },
+    {
+        id: '004',
+        itemImage: './images/item-26.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42
+    },
+    {
+        id: '005',
+        itemImage: './images/item-19.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42
+    },
+    {
+        id: '006',
+        itemImage: './images/item-21.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42
+    },
+    
+]
