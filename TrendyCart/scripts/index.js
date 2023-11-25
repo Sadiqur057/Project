465b704d315ea265acb79c42c35b987f798c3c27
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-25 18:37:57 +0600

    dynamically sending data

---
+++ TrendyCart/scripts/index.js
@@ -0,0 +1,32 @@
+function displayItemOnHomePage(){
+    let itemsContainerElement = document.querySelector('#items-container');
+let innerHTML = '';
+items.forEach(item =>{
+    innerHTML+= `
+        <div class="item-content">
+        <div class="item-image-container">
+            <img class="item-image" src="${item.itemImage}" alt="">
+        </div>
+        <div class="rating">
+            <p class="rating-text"> ${item.rating.star} <i class="fa-solid fa-star rating-icon"></i>  | ${item.rating.numOfRevies}</p>
+        </div>
+        <div class="item-desc">
+            <h4 class="company-name">${item.companyName}</h4>
+            <p class="item-name">${item.itemName}</p>
+            <div class="price-section">
+                <span class="current-price">BDT. ${item.currentPrice}</span>
+                <span class="original-price">BDT. ${item.originalPrice}</span>
+                <span class="discount">${item.discount}% OFF</span>
+            </div>
+        </div>
+        <div class="bag-container">
+            <a href="#" onclick = "addToBag(${item.id})" class="add-to-bag"><span class="material-symbols-outlined bag-icon">shopping_bag </span> Add to Bag
+            </a>
+            <p class="item-size">Sizes: L, XL</p>
+        </div>
+        </div>
+`
+});
+itemsContainerElement.innerHTML = innerHTML;
+}
+displayItemOnHomePage();
